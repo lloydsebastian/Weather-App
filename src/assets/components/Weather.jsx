a0777d659ca5dd32ba7ec97ed903dde49ac18dc3
Lloyd Sebastian <lloydseban@gmail.com>
2024-06-23 16:25:11 +0530

    Completed User Interface

--- src/assets/components/Weather.jsx
+++ src/assets/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Weather.css'
 import search_icon from '../search.png'
 import clear_icon from '../clear.png'
@@ -9,7 +9,25 @@ import rain_icon from '../rain.png'
 import snow_icon from '../snow.png'
 import wind_icon from '../wind.png'
 
+
 const Weather = () => {
+
+    const search = async (city)=>{
+        try{
+            const url='https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}';
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log(data)
+        
+        } catch (error){
+
+        }
+    }
+
+    useEffect(()=>{
+        search("London")
+    },[])
+
   return (
     <div className='weather'>
         <div className='search-bar'>
@@ -19,6 +37,22 @@ const Weather = () => {
         <img src={clear_icon} alt="" className='weather-icon'/>
         <p className='temperature'>16°c</p>
         <p className='location'>London</p>
+        <div className='weather-data'>
+            <div className='col'>
+                <img src={humidity_icon} alt="" />
+                <div>
+                    <p>91 %</p>
+                    <span>Humidity</span>
+                </div>
+            </div>
+            <div className='col'>
+                <img src={wind_icon} alt="" />
+                <div>
+                    <p>3.6 km/h</p>
+                    <span>Wind</span>
+                </div>
+            </div>
+    </div>
     </div>
   )
 }
